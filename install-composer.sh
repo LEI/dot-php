41d74a1154973ba3420cbe32e278114da0b3231b
LEI <guillaume.frichet@gmail.com>
2018-08-05 13:17:30 +0200

    +

--- install-composer.sh
+++ install-composer.sh
@@ -14,17 +14,15 @@ if ! hash composer 2> /dev/null; then
   sig=$(cat /tmp/composer-installer.sig)
   sum="$(sha384sum /tmp/composer-installer.php | cut -d' ' -f1)"
   # sha384: $sig /tmp/composer-installer.php
-  if [ "$sig" = "$sum" ]; then
+  if [ "$sig" != "$sum" ]; then
     echo "sha384 sum mismatch"
     exit 1
   fi
   chmod 0755 /tmp/composer-installer.php
   php /tmp/composer-installer.php --install-dir=/usr/local/bin --filename=composer # --version=
+else
+  composer self-update
 fi
-
-composer self-update
-
-# export PATH=$PATH:~/.composer/vendor/bin
 
 # for name in "$@"; do
 #   composer global require "$name" # @stable
