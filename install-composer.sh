5916455126537ac5d9ea029f7143a5f8a77a6f0d
LEI <guillaume.frichet@gmail.com>
2018-08-05 13:28:24 +0200

    f

--- install-composer.sh
+++ install-composer.sh
@@ -3,31 +3,55 @@
 set -e
 
 if ! hash php 2> /dev/null; then
-  echo "PHP does not seem installed"
+  1>&2 echo "php: command not found"
   exit 1
 fi
 
-if ! hash composer 2> /dev/null; then
-  curl -sSL https://composer.github.io/installer.sig -o /tmp/composer-installer.sig
-  curl -sSL https://getcomposer.org/installer -o /tmp/composer-installer.php
-  ! sha384sum 2> /dev/null && alias sha384sum="sha --alorigthm 384"
+install_composer() {
+  curl -sSL https://composer.github.io/installer.sig \
+    -o /tmp/composer-installer.sig
   sig=$(cat /tmp/composer-installer.sig)
+
+  curl -sSL https://getcomposer.org/installer \
+    -o /tmp/composer-installer.php
+  sha384sum 2> /dev/null || alias sha384sum="sha --alorigthm 384"
   sum="$(sha384sum /tmp/composer-installer.php | cut -d' ' -f1)"
+
   # sha384: $sig /tmp/composer-installer.php
   if [ "$sig" != "$sum" ]; then
-    echo "sha384 sum mismatch"
+    1>&2 echo "sha384 sum mismatch"
     exit 1
   fi
+
   chmod 0755 /tmp/composer-installer.php
-  php /tmp/composer-installer.php --install-dir=/usr/local/bin --filename=composer # --version=
-else
-  composer self-update
-fi
+  php /tmp/composer-installer.php \
+    --install-dir=/usr/local/bin \
+    --filename=composer # --version=
+}
 
-# for name in "$@"; do
-#   composer global require "$name" # @stable
-# done
+composer_self_update() {
+  # --quiet
+  composer --no-interaction \
+    self-update --no-progress
+}
 
-if [ $# -ne 0 ]; then
-  echo composer global require "$@"
-fi
+composer_global_require() {
+  # [ $# -ne 0 ]
+  for name in "$@"; do
+    # --quiet
+    composer --no-interaction \
+      global --no-progress \
+      require "$name" # @stable
+  done
+}
+
+do_install() {
+  if ! hash composer 2> /dev/null; then
+    install_composer
+  else
+    composer_self_update
+  fi
+  composer_global_require "$@"
+}
+
+do_install "$@"
